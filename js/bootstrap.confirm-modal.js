7069861b8160916fb8ed2ebaa8b7fc7bc53adf5c
Christophe Gosiau <christophe@gosiau.be>
2018-11-15 11:31:06 +0100

    Update bootstrap.confirm-modal.js
    prevent double confirmation dialog

--- js/bootstrap.confirm-modal.js
+++ js/bootstrap.confirm-modal.js
@@ -1,6 +1,7 @@
 $(function ( $ ) {
 
 	$.fn.confirmModal = function() {
+		$(this).unbind('click');		
 		$(this).click(function(ev) {
 
 			template = '<div id="dataConfirmModal" class="modal fade" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">%%title%%</h4><button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button></div><div class="modal-body"><p>%%message%%</p></div><div class="modal-footer"><a class="btn btn-primary" href="%%btn_href%%" id="data-confirm-ok">%%btn_ok%%</a><a class="btn btn-default" data-dismiss="modal" aria-hidden="true" id="data-confirm-cancel">%%btn_cancel%%</a></div></div></div></div>';
